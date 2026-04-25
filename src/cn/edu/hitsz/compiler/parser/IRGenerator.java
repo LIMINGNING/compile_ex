226566dcd8e11226913bab8e190dc88e5a9158aa
MINGNING <2664135882@qq.com>
2026-04-25 16:19:51 +0800

    finish lab3

--- src/cn/edu/hitsz/compiler/parser/IRGenerator.java
+++ src/cn/edu/hitsz/compiler/parser/IRGenerator.java
@@ -1,13 +1,19 @@
 package cn.edu.hitsz.compiler.parser;
 
-import cn.edu.hitsz.compiler.NotImplementedException;
+import cn.edu.hitsz.compiler.ir.IRImmediate;
+import cn.edu.hitsz.compiler.ir.IRValue;
+import cn.edu.hitsz.compiler.ir.IRVariable;
 import cn.edu.hitsz.compiler.ir.Instruction;
+import cn.edu.hitsz.compiler.ir.InstructionKind;
 import cn.edu.hitsz.compiler.lexer.Token;
 import cn.edu.hitsz.compiler.parser.table.Production;
 import cn.edu.hitsz.compiler.parser.table.Status;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 import cn.edu.hitsz.compiler.utils.FileUtils;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 // TODO: 实验三: 实现 IR 生成
@@ -16,39 +22,123 @@ import java.util.List;
  *
  */
 public class IRGenerator implements ActionObserver {
+    private final Deque<Attribute> stack = new ArrayDeque<>();
+    private final List<Instruction> instructions = new ArrayList<>();
 
     @Override
     public void whenShift(Status currentStatus, Token currentToken) {
         // TODO
-        throw new NotImplementedException();
+        stack.push(Attribute.token(currentToken));
     }
 
     @Override
     public void whenReduce(Status currentStatus, Production production) {
         // TODO
-        throw new NotImplementedException();
+        switch (production.index()) {
+            case 6 -> reduceAssign();
+            case 7 -> reduceReturn();
+            case 8 -> reduceBinary(InstructionKind.ADD);
+            case 9 -> reduceBinary(InstructionKind.SUB);
+            case 10, 12 -> reduceSingle();
+            case 11 -> reduceBinary(InstructionKind.MUL);
+            case 13 -> reduceParentheses();
+            case 14 -> reduceIdentifier();
+            case 15 -> reduceImmediate();
+            default -> reduceIgnored(production);
+        }
     }
 
 
     @Override
     public void whenAccept(Status currentStatus) {
         // TODO
-        throw new NotImplementedException();
+        stack.clear();
     }
 
     @Override
     public void setSymbolTable(SymbolTable table) {
         // TODO
-        throw new NotImplementedException();
     }
 
     public List<Instruction> getIR() {
         // TODO
-        throw new NotImplementedException();
+        return List.copyOf(instructions);
     }
 
     public void dumpIR(String path) {
         FileUtils.writeLines(path, getIR().stream().map(Instruction::toString).toList());
     }
-}
 
+    private void reduceAssign() {
+        final var source = stack.pop();
+        stack.pop();
+        final var target = stack.pop();
+        instructions.add(Instruction.createMov(IRVariable.named(target.token.getText()), source.value));
+        stack.push(Attribute.empty());
+    }
+
+    private void reduceReturn() {
+        final var returnValue = stack.pop();
+        stack.pop();
+        instructions.add(Instruction.createRet(returnValue.value));
+        stack.push(Attribute.empty());
+    }
+
+    private void reduceBinary(InstructionKind kind) {
+        final var rhs = stack.pop();
+        stack.pop();
+        final var lhs = stack.pop();
+        final var result = IRVariable.temp();
+
+        switch (kind) {
+            case ADD -> instructions.add(Instruction.createAdd(result, lhs.value, rhs.value));
+            case SUB -> instructions.add(Instruction.createSub(result, lhs.value, rhs.value));
+            case MUL -> instructions.add(Instruction.createMul(result, lhs.value, rhs.value));
+            default -> throw new RuntimeException("Unsupported binary instruction kind: " + kind);
+        }
+
+        stack.push(Attribute.value(result));
+    }
+
+    private void reduceSingle() {
+        stack.push(stack.pop());
+    }
+
+    private void reduceParentheses() {
+        stack.pop();
+        final var value = stack.pop();
+        stack.pop();
+        stack.push(value);
+    }
+
+    private void reduceIdentifier() {
+        final var id = stack.pop();
+        stack.push(Attribute.value(IRVariable.named(id.token.getText())));
+    }
+
+    private void reduceImmediate() {
+        final var immediate = stack.pop();
+        stack.push(Attribute.value(IRImmediate.of(Integer.parseInt(immediate.token.getText()))));
+    }
+
+    private void reduceIgnored(Production production) {
+        for (int i = 0; i < production.body().size(); i++) {
+            stack.pop();
+        }
+        stack.push(Attribute.empty());
+    }
+
+    private record Attribute(Token token, IRValue value) {
+        static Attribute empty() {
+            return new Attribute(null, null);
+        }
+
+        static Attribute token(Token token) {
+            return new Attribute(token, null);
+        }
+
+        static Attribute value(IRValue value) {
+            return new Attribute(null, value);
+        }
+    }
+}
--- src/cn/edu/hitsz/compiler/parser/SemanticAnalyzer.java
+++ src/cn/edu/hitsz/compiler/parser/SemanticAnalyzer.java
@@ -1,37 +1,72 @@
 package cn.edu.hitsz.compiler.parser;
 
-import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.lexer.Token;
 import cn.edu.hitsz.compiler.parser.table.Production;
 import cn.edu.hitsz.compiler.parser.table.Status;
+import cn.edu.hitsz.compiler.symtab.SourceCodeType;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 // TODO: 实验三: 实现语义分析
 public class SemanticAnalyzer implements ActionObserver {
+    private SymbolTable symbolTable;
+    private final Deque<Attribute> stack = new ArrayDeque<>();
 
     @Override
     public void whenAccept(Status currentStatus) {
         // TODO: 该过程在遇到 Accept 时要采取的代码动作
-        throw new NotImplementedException();
+        stack.clear();
     }
 
     @Override
     public void whenReduce(Status currentStatus, Production production) {
         // TODO: 该过程在遇到 reduce production 时要采取的代码动作
-        throw new NotImplementedException();
+        switch (production.index()) {
+            case 4 -> {
+                final var id = stack.pop();
+                final var type = stack.pop();
+                symbolTable.get(id.token.getText()).setType(type.type);
+                stack.push(Attribute.empty());
+            }
+            case 5 -> {
+                stack.pop();
+                stack.push(Attribute.type(SourceCodeType.Int));
+            }
+            default -> {
+                for (int i = 0; i < production.body().size(); i++) {
+                    stack.pop();
+                }
+                stack.push(Attribute.empty());
+            }
+        }
     }
 
     @Override
     public void whenShift(Status currentStatus, Token currentToken) {
         // TODO: 该过程在遇到 shift 时要采取的代码动作
-        throw new NotImplementedException();
+        stack.push(Attribute.token(currentToken));
     }
 
     @Override
     public void setSymbolTable(SymbolTable table) {
         // TODO: 设计你可能需要的符号表存储结构
         // 如果需要使用符号表的话, 可以将它或者它的一部分信息存起来, 比如使用一个成员变量存储
-        throw new NotImplementedException();
+        this.symbolTable = table;
+    }
+
+    private record Attribute(Token token, SourceCodeType type) {
+        static Attribute empty() {
+            return new Attribute(null, null);
+        }
+
+        static Attribute token(Token token) {
+            return new Attribute(token, null);
+        }
+
+        static Attribute type(SourceCodeType type) {
+            return new Attribute(null, type);
+        }
     }
 }
-
